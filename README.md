7e097630654a2a3d566d44778a7fed437dc4530b
Alexander Rodriguez <98533790+AlexDoesCS@users.noreply.github.com>
2022-03-29 16:18:12 +0000

    Code

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+This purpose of this lab was to use the formula 3n+1 and make a graph where a user inputs a numbers and it'll graph all the loop values and iterations. It'll also show what the Maximum, so far, is.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 1
 
-Grace days remaining: < # >/5
+Grace days remaining: 4/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ None
 
 ## REFERENCES:
- < List any outside resources used >
+ None
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+None
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 
 def seq3np1(n):
     """
@@ -5,14 +6,48 @@ def seq3np1(n):
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
+    
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
+        count +=1
+    return count
+ 
+#Part B
+def graph(upper):
+  window = turtle.Screen()
+  alex = turtle.Turtle()
+  squirtle = turtle.Turtle()
+  window.setworldcoordinates(0, 0, 10, 10)
+  max_so_far=0
+  for start in range(1, upper +1):
+    result = seq3np1(start)
+    if (max_so_far < result):
+      max_so_far = result
+      squirtle.clear()
+      squirtle.up()
+      squirtle.goto(0, result)
+      squirtle.write("Maximum so far: " +str(result))
+      window.setworldcoordinates(0, 0, start+10, result+10)
+    alex.goto(start, result)
+  window.exitonclick()
 
+
+#Part A
 def main():
-	seq3np1(3)
+  upper = int(input("What is the Upperbouund Value?"))
+  if(upper < 0):
+    exit()
+  else:
+    for start in range(1, upper +1):
+      print(start, "Is the current loop value")
+      iterations = seq3np1(start)
+      print(iterations, "Is the number of Iterations")
+
+  graph(upper)
+  seq3np1(3)
 main()
+
